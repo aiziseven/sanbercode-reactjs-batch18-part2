5a0be002e4ae9bb8b1e5b91ae6282fd3e638e8d6
aiziseven <zuliardiirfan@gmail.com>
2020-10-12 21:35:46 +0700

    update tugas 11 pekan 3

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Form from './Tugas-9/Form';
 import Table from './Tugas-10/Table';
+import Timer from './Tugas-11/Timer';
 
 function App() {
   return (
     <div>
       <Table />
       <Form />
+      <Timer />
     </div>
   );
 }
---
+++ src/Tugas-11/Timer.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react"
+
+class Timer extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            time: 100,
+            showTime: true,
+            date: new Date(),
+        }
+    }
+
+    componentDidMount() {
+        if (this.props.start !== undefined) {
+            this.setState({ time: this.props.start })
+        }
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        console.log('unmount')
+        clearInterval(this.timerID);
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.time === 0) {
+            this.componentWillUnmount()
+            console.log(prevState.showTime);
+            if (prevState.showTime === this.state.showTime) {
+                this.hideTime()
+            }
+        }
+    }
+
+    tick() {
+        this.setState({
+            time: this.state.time - 1,
+            date: new Date(),
+        });
+    }
+
+    hideTime() {
+        this.setState({ showTime: false })
+    }
+
+    render() {
+        return (
+            <>
+                {
+                    this.state.showTime && (
+                        <div style={{ textAlign: 'center' }} id='timer'>
+                            <h1>
+                                <span>sekarang jam: {this.state.date.toLocaleTimeString()}</span>
+                                <span>&nbsp;&nbsp;&nbsp;</span>
+                                <span>hitung mundur: {this.state.time}</span>
+                            </h1>
+                        </div>
+                    )
+                }
+
+            </>
+            // <div style={{ textAlign: 'center' }} id='timer'>
+            //     <h1>
+            //         <span>sekarang jam: {this.state.date.toLocaleTimeString()}</span>
+            //         <span>&nbsp;&nbsp;&nbsp;</span>
+            //         <span>hitung mundur: {this.state.showTime && this.state.time}</span>
+            //     </h1>
+            // </div>
+        )
+    }
+}
+
+export default Timer
